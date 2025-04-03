4cb668730c59d1e7c073fa273c08210d9a6d0109
whale-coding <njq3699@gmail.com>
2025-04-03 15:20:45 +0800

    nacos配置中心-配置监听

--- springcloud_shangguigu/cloud-demo/services/service-order/src/main/java/com/star/OrderApplication.java
+++ springcloud_shangguigu/cloud-demo/services/service-order/src/main/java/com/star/OrderApplication.java
@@ -1,8 +1,17 @@
 package com.star;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @EnableDiscoveryClient  // 开启nacos服务发现功能
 @SpringBootApplication
@@ -11,4 +20,37 @@ public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
+
+
+    /**
+     * 配置监听
+     * 1.项目启动就监听配置文件变化
+     * 2.发生变化后拿到变化值
+     * 3.发送邮件通知
+     * 项目一启动就会运行下面这个方法
+     */
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager){
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                ConfigService configService = nacosConfigManager.getConfigService();  // 获取配置服务
+                configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                    @Override
+                    public Executor getExecutor() {
+                        return Executors.newFixedThreadPool(4);
+                    }
+
+                    @Override
+                    public void receiveConfigInfo(String configInfo) {
+                        System.out.println("变化的配置信息:" + configInfo);
+
+                        System.out.println("邮件通知...");
+                    }
+                });
+                System.out.println("========================");
+            }
+        };
+    }
+
 }
