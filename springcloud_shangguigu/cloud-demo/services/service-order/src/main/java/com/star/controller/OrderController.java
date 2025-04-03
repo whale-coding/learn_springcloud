ff79cb7a2e0bfd4c25e28eff88c3e9d0e435a446
whale-coding <njq3699@gmail.com>
2025-04-03 14:43:14 +0800

    nacos配置中心-基本用法

--- springcloud_shangguigu/cloud-demo/services/service-order/src/main/java/com/star/controller/OrderController.java
+++ springcloud_shangguigu/cloud-demo/services/service-order/src/main/java/com/star/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.star.controller;
 import com.star.order.bean.Order;
 import com.star.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,23 @@ import javax.lang.model.type.ReferenceType;
  * @Author: 聂建强
  * @Description: 订单控制器
  */
+@RefreshScope  // 激活配置属性的刷新功能，用于nacos做配置中心，自动刷新配置
 @RestController
 public class OrderController {
 
     @Resource
     private OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    // 测试nacos作为配置中心
+    @GetMapping("/config")
+    public String config(){
+        return "order.timeout=" + orderTimeout + ": order.auto-confirm=" + orderAutoConfirm;
+    }
 
     // 创建订单
     @GetMapping("/create")
