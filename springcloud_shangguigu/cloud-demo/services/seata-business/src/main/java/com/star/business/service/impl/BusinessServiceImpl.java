11f332cae39b9e276475d4c8b74cccb637162002
whale-coding <njq3699@gmail.com>
2025-04-04 22:31:43 +0800

    Seata-解决分布式事务问题

--- springcloud_shangguigu/cloud-demo/services/seata-business/src/main/java/com/star/business/service/impl/BusinessServiceImpl.java
+++ springcloud_shangguigu/cloud-demo/services/seata-business/src/main/java/com/star/business/service/impl/BusinessServiceImpl.java
@@ -4,6 +4,7 @@ import com.star.business.feign.OrderFeignClient;
 import com.star.business.feign.StorageFeignClient;
 import com.star.business.service.BusinessService;
 import jakarta.annotation.Resource;
+import org.apache.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 
@@ -16,6 +17,7 @@ public class BusinessServiceImpl implements BusinessService {
     @Resource
     private OrderFeignClient orderFeignClient;
 
+    @GlobalTransactional  // seata的全局事务，用于解决分布式事务
     @Override
     public void purchase(String userId, String commodityCode, int orderCount) {
         //1. 扣减库存
